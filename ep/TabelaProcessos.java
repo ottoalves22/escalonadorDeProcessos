21008317c88caf6c53a4b817388af14bba93f46a
ottoalves22 <ottoalves22@gmail.com>
2021-10-13 21:22:08 -0300

    precisa dar uma CHECADA nos comparable da linha 13 do TabelaProcessos.java

--- ep/TabelaProcessos.java
+++ ep/TabelaProcessos.java
@@ -1,15 +1,27 @@
 package ep;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class TabelaProcessos {
-	SortedList processos_prontos = new SortedList();
-	SortedList processos_bloqueados = new SortedList();	
+	ArrayList processos_prontos = new ArrayList<BCP>();
+	ArrayList processos_bloqueados = new ArrayList<BCP>();
+
+	// alterar metodo void inserirOrdenado(BCP processo)
+	public void inserirOrdenado(BCP processo, ArrayList fila_processo){
+		fila_processo.add(processo);
+		Comparable<BCP> cmp = (Comparable<BCP>)processo;
+		for (int i = size() - 1; i > 0 && cmp.compareTo(get(i - 1)) < 0; i--){
+			Collections.swap(this, i, i - 1);
+		}
+	}
 
 	public void adicionaProcessoPronto(BCP bloco) {
-		this.processos_prontos.insertSorted(bloco);
+		inserirOrdenado(bloco, processos_prontos);
 	}
 	
 	public void adicionaProcessoBloqueado (BCP bloco) {
-		this.processos_bloqueados.insertSorted(bloco);
+		this.processos_bloqueados.inserirOrdenado(bloco);
 	}
 	
 	public BCP removePrimeiroPronto() {
